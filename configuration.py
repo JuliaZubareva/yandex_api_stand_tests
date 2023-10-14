865a46307bdd23430953b20495b2d21e5207ac4d
User@DESKTOP-U46M2V7 <juliaolk9@yandex.ru>
2023-10-14 15:29:15 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://47e394ce-1289-4a99-ae55-af36b28e2b2c.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+OG_MAIN_PATH = "/api/logs/main/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,12 @@
+import configuration
+import requests
+import data
+
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+
+
+response = get_logs()
+print(response.status_code)
+print(response.headers)
